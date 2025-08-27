b13e0d9e53a9681569c0e7a95594326b773c6c22
Ziyad Alwafie <zyalwfie@gmail.com>
2025-08-27 11:27:45 +0800

    webstore(cart system): #25 basic checkout fields & summaries

--- app/Livewire/Checkout.php
+++ app/Livewire/Checkout.php
@@ -2,20 +2,79 @@
 
 namespace App\Livewire;
 
-use Illuminate\Support\Facades\Gate;
+use App\Data\CartData;
 use Livewire\Component;
+use Illuminate\Support\Number;
+use Illuminate\Support\Facades\Gate;
+use App\Contract\CartServiceInterface;
+use Livewire\Attributes\Title;
 
+#[Title('Webstore | Checkout')]
 class Checkout extends Component
 {
+    public array $data = [
+        'full_name' => null,
+        'email' => null,
+        'phone' => null,
+        'address_line' => null,
+    ];
+
+    public array $summaries = [
+        'sub_total' => 0,
+        'sub_total_formatted' => '-',
+        'shipping_total' => 0,
+        'shipping_total_formatted' => '-',
+        'grand_total' => 0,
+        'grand_total_formatted' => '-',
+    ];
+
     public function mount()
     {
         if (!Gate::inspect('is_stock_available')->allowed()) {
             return redirect()->route('cart');
         }
+        $this->calculateTotal();
+    }
+
+    public function rules()
+    {
+        return [
+            'data.full_name' => ['required', 'min:3', 'max:255'],
+            'data.email' => ['required', 'email', 'max:255'],
+            'data.phone' => ['required', 'min:9', 'max:13'],
+            'data.shipping_line' => ['required', 'min:10', 'max:255']
+        ];
+    }
+
+    public function calculateTotal()
+    {
+        data_set($this->summaries, 'sub_total', $this->cart->total);
+        data_set($this->summaries, 'sub_total_formatted', $this->cart->total_formatted);
+
+        $shipping_cost = 0;
+        data_set($this->summaries, 'shipping_total', $shipping_cost);
+        data_set($this->summaries, 'shipping_total_formatted', Number::currency($shipping_cost));
+
+        $grand_total = $this->cart->total + $shipping_cost;
+        data_set($this->summaries, 'grand_total', $grand_total);
+        data_set($this->summaries, 'grand_total_formatted', Number::currency($grand_total));
+    }
+
+    public function getCartProperty(CartServiceInterface $cart): CartData
+    {
+        return $cart->all();
+    }
+
+    public function placeAnOrder()
+    {
+        $this->validate();
+        dd($this->data);
     }
 
     public function render()
     {
-        return view('livewire.checkout');
+        return view('livewire.checkout', [
+            'cart' => $this->cart
+        ]);
     }
 }
--- resources/views/components/single-product-list.blade.php
+++ resources/views/components/single-product-list.blade.php
@@ -1,17 +1,19 @@
+@props(['cart_item'])
+
 <div class="flex items-center gap-2 border-b border-gray-200">
     <div class="relative overflow-hidden rounded-md h-15 w-15">
         <img class="object-coversize-full"
-            src="https://images.unsplash.com/photo-1546087513-2a2bc7fb6fa9?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Product Name">
+            src="{{ $cart_item->product()->imgUrl }}"
+            alt="{{ $cart_item->product()->name }}">
     </div>
     <div class="flex items-center">
         <div class="py-2">
             <h3 class="text-gray-800 text-md dark:text-white">
-                Product Name
+                {{ $cart_item->product()->name }}
             </h3>
-            <h2 class="text-sm text-gray-500">Ebook, Software Engineer, Web Developer</h2>
+            <h2 class="text-sm text-gray-500">{{ $cart_item->product()->short_desc }}</h2>
             <p class="mt-1 text-sm text-black text-md dark:text-black">
-                Rp.123.456 x 3
+                {{ $cart_item->product()->formattedPrice }} x {{ $cart_item->quantity }}
             </p>
         </div>
     </div>

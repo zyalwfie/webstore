d9cb4607fb0fc2f6663d5592bcdfc0d9f4938d7f
wafy <zyalwfie@gmail.com>
2026-01-12 03:28:14 +0800

    webstore(checkout): #50 install filament breezy

--- app/Providers/Filament/BackPanelProvider.php
+++ app/Providers/Filament/BackPanelProvider.php
@@ -17,6 +17,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Jeffgreco13\FilamentBreezy\BreezyCore;
 
 class BackPanelProvider extends PanelProvider
 {
@@ -53,6 +54,17 @@ class BackPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                BreezyCore::make()
+                ->myProfile(
+                    shouldRegisterUserMenu: true, // Sets the 'account' link in the panel User Menu (default = true)
+                    userMenuLabel: 'My Profile', // Customizes the 'account' link label in the panel User Menu (default = null)
+                    shouldRegisterNavigation: false, // Adds a main navigation item for the My Profile page (default = false)
+                    navigationGroup: 'Settings', // Sets the navigation group for the My Profile page (default = null)
+                    hasAvatars: false, // Enables the avatar upload form component (default = false)
+                    slug: 'my-profile' // Sets the slug for the profile page (default = 'my-profile')
+                )
             ]);
     }
 }

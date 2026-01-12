1a731e8dc6eec87792e9b1c27ea8951e6dd62536
wafy <zyalwfie@gmail.com>
2026-01-12 15:07:55 +0800

    webstore(checkout): #51 user resources

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Models\User;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Support\Facades\Hash;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255)
+                    ->inlineLabel(),
+                TextInput::make('email')
+                    ->email()
+                    ->unique(ignoreRecord: true)
+                    ->required()
+                    ->maxLength(255)
+                    ->inlineLabel(),
+                Fieldset::make('password')
+                    ->schema([
+                        TextInput::make('password')
+                            ->password()
+                            ->revealable()
+                            ->maxLength(255)
+                            ->required(fn($record) => $record === null)
+                            ->dehydrateStateUsing(fn($state) => !empty($state) ? Hash::make($state) : ''),
+                        TextInput::make('confirm_password')
+                            ->same('password')
+                            ->password()
+                            ->revealable()
+                            ->maxLength(255)
+                            ->required(fn($record) => $record === null)
+                            ->dehydrated(false)
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('email'),
+                TextColumn::make('created_at'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
